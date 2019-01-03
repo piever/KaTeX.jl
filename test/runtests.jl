df0588e2e6e9e5f0fa67fbaef5c4dba3b1e5e479
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-01-03 12:38:07 +0000

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,41 @@
 using KaTeX
 using Test
+
+@testset "deps" begin
+    for fn in ["katex.min.js", "katex.min.css", "auto-render.min.js"]
+        @test isfile(joinpath(@__DIR__, "..", "assets", fn))
+    end
+end
+
+const katexfonts = [
+    "KaTeX_AMS-Regular",
+    "KaTeX_Caligraphic-Bold",
+    "KaTeX_Caligraphic-Regular",
+    "KaTeX_Fraktur-Bold",
+    "KaTeX_Fraktur-Regular",
+    "KaTeX_Main-Regular",
+    "KaTeX_Main-Bold",
+    "KaTeX_Main-Italic",
+    "KaTeX_Main-BoldItalic",
+    "KaTeX_Math-Italic",
+    "KaTeX_Math-BoldItalic",
+    "KaTeX_SansSerif-Bold",
+    "KaTeX_SansSerif-Italic",
+    "KaTeX_SansSerif-Regular",
+    "KaTeX_Script-Regular",
+    "KaTeX_Size1-Regular",
+    "KaTeX_Size2-Regular",
+    "KaTeX_Size3-Regular",
+    "KaTeX_Size4-Regular",
+    "KaTeX_Typewriter-Regular",
+]
+
+const exts =["ttf", "woff", "woff2"]
+
+@testset "katexfonts" begin
+    for font in katexfonts
+        for ext in exts
+            @test isfile(joinpath(@__DIR__, "..", "assets", "fonts", join([font, ext], '.')))
+        end
+    end
+end
